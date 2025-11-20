8eed4c0f2ff77ace6a5d6db55b4518a28362b4f3
foadm <foad_momtazi@hotmail.com>
2025-11-20 20:45:17 +0000

    added scaffold

--- comms/nats.go
+++ comms/nats.go
@@ -13,11 +13,19 @@ type natsConfigType struct {
 	URL string `json:"url"`
 }
 
+type channelType struct {
+	ID         string
+	channel    chan string
+	subscriber *nats.Subscription
+}
+
+var channels map[string]channelType = make(map[string]channelType)
+
 var nc *nats.Conn
 var natsConfig natsConfigType = natsConfigType{}
 
 // ======================================================
-//
+// init initializes package-level variables
 // ======================================================
 func init() {
 
@@ -50,12 +58,71 @@ func Init(config string) {
 	}
 }
 
+// ======================================================
+// Close closes the NATS connection
+// ======================================================
 func Close() {
 	nc.Close()
 }
 
+// ======================================================
+// Send sends a message to a specified channel. This is
+// different from publishing to a queue group and only.
+// this message will go to all subscribers of the channel.
+// ======================================================
 func Send(message string, channel string) error {
 	fmt.Println("Send message:", message)
 
-	return nc.Publish("simplePublisher", []byte("Hello World"))
+	return nc.Publish("simplePublisher", []byte(message))
+}
+
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// ======================================================
+// Subscribe subscribes to a specified channel with a
+// ======================================================
+func Subscribe(channel string, subChannel chan string) error {
+	var _handler nats.MsgHandler = func(msg *nats.Msg) {
+		subChannel <- string(msg.Data)
+	}
+	_subs, _err := nc.Subscribe(channel, _handler)
+	if _err == nil {
+		channels[channel] = channelType{
+			ID:         channel,
+			channel:    subChannel,
+			subscriber: _subs,
+		}
+	}
+	if _err != nil {
+		fmt.Printf("Subscribe error: %v\n", _err)
+		return _err
+	}
+
+	return _err
+}
+
+// ======================================================
+// handler for subscriber messages
+// ======================================================
+func asyncMessageHandler(msg *nats.Msg) {
+	fmt.Printf("Received a message: %s\n", string(msg.Data))
+}
+
+// ======================================================
+// listen subscribes to a specified channel with a
+// ======================================================
+func listen(channel string, handler nats.MsgHandler) (*nats.Subscription, error) {
+	return nc.QueueSubscribe(channel, "queueGroup", handler)
 }
