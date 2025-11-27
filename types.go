8e0b0c9ebf824209dd87d5fde63bbb050a2e4cca
foadm <foad_momtazi@hotmail.com>
2025-11-27 13:31:07 +0000

    Refactor types and constants for main package

--- types.go
+++ types.go
@@ -1,36 +1,73 @@
-package raft
+package main
 
+// heartbeat message (heartbeatRequestId)           standard message
+// heartbeatResponse message (heartbeatResponseId)  standard message with status in Data
+// nomination message (nominationtId)               standard message with host id in Data
+// vote message (electionVoteId)                    standard message with candidate ID in Data
+// inaugration message (inaugurationId)             standard message with new leader ID in Data
+
+// ======================================================
+// Constants for requestIds in the genericMessage field of
+// type genericMessage
+// ======================================================
 const (
-	Unknown  string = "unknown"
-	Follower string = "follower"
-	Leader   string = "leader"
-	Election string = "election"
+	heartbeatRequestId    = "ping"
+	heartbeatResponseId   = "pong"
+	nominationtRequestId  = "nomination"
+	electionVoteId        = "myVote"
+	inaugurationRequestId = "inauguration"
 )
 
-// ===========================================================================
-// This will be used in a map to keep the
-// ===========================================================================
+// ======================================================
+// Communication channels
+// ======================================================
+const (
+	heartbeatChannel         = heartbeatRequestId
+	heartbeatResponseChannel = heartbeatResponseId
+	electionChannel          = "election"
+)
+const (
+	heartbeatInterval        = 1000
+	heartbeatTimeoutInterval = 3 * heartbeatInterval
+	electionTimeoutInterval  = 10 * heartbeatInterval
+)
+
+// ======================================================
+// Node modes
+// ======================================================
+const (
+	UNKNOWN  = "UNKNOWN"
+	FOLLOWER = "FOLLOWER"
+	LEADER   = "LEADER"
+	ELECTION = "ELECTION"
+)
+
+const (
+	NO_RESPONSE = "noResponse"
+	HEALTHY     = "healthy"
+	DEAD        = "sick"
+)
+
+// ======================================================
+// This will be used in a map to keep the node information
+// ======================================================
 type nodeHost struct {
-	ProcessId string `json: "processId"`
-	IP        string `json: "ip"`      // IP address
-	Hostname  string `json "hostname"` // hostname of the node
+	Hostname  string `json:"hostname"` // hostname of the node
+	ProcessId string `json:"processId"`
+	UniqueId  string `json:"uniqueId"`
 }
 
-type nodesStatus struct {
-	Id     string   `json: "id"`
-	node   nodeHost `json: "node"`
-	Status string   `json: "status"` // live or not responding"
-	Mode   string   `json: "mode"`   // is it leader, follower or election
+type genericMessage struct {
+	RequestId string   `json:"requestId"` // eg "heartbeat", "nomination", "vote"
+	Data      string   `json:"data"`      // optional data
+	Sender    nodeHost `json:"sender"`
+	Timestamp string   `json:"timestamp"`
 }
 
-type request struct {
-	RequestId string   `json:"requestId"` // optional Id, like unique trans ID
-	Request   string   `json: "request"`
-	Sender    nodeHost `json: "sender"`
-}
-
-type response struct {
-	RequestId string   `json:"requestId"` // optional Id, like unique trans ID
-	Data      string   `json: "data"`     //    string   `json: "request"`
-	Sender    nodeHost `json: "sender"`
-}
+// ======================================================
+// This is for keeping track of the nodes status by the leader
+// ======================================================
+// type nodesMode struct {
+// 	genericMessage
+// 	Mode string `json: "mode"` // is it leader, follower or election
+// }
